fb3755d08b50f59b4d3bddade4c011573d4e46ad
dellta103 <95919228+dellta103@users.noreply.github.com>
2022-01-03 23:09:14 +0700

    second

--- bai5.js
+++ bai5.js
@@ -1,26 +1,22 @@
 var inputn5El=document.getElementById("inputn5")
-var i5=1
 function bai5(){
     var inputn5=inputn5El.value*1
-   console.log(i5);
-   for( i5=2;i5<=inputn5;i5++){
-       if(i5==2){
-           console.log(i5);
-       }else if(i5%2==0){
-           console.log("no");
-       }else{
-        for (var k5 = 3; k5 < i5-1; k5+=2)
-        {
-            
-            if (i5 % k5 == 0){
-                // console.log("no");
-            }else{
-                console.log(i5);
-            }
-        }
-       }
+    
+    for(var k=1;k<=inputn5;k++){
+    var flag=true
 
-       
-       
-}}
+    if(k<2){
+        flag=true
+    }else{
+        for (var i5=2;i5<k;i5++){
+            if(k%i5==0){
+                flag=false
+                break
+            }
+        }   
+    }
+    if(flag==true){
+        console.log(k);
+    } }}
+    
     
